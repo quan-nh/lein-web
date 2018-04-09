059289fd2cb20f4f892dd0c1fd207e92b60f09f6
Quan <huu2uan@gmail.com>
2018-04-09 09:57:37 +0700

    cleanup

--- src/leiningen/new/simple_web.clj
+++ src/leiningen/new/simple_web.clj
@@ -19,16 +19,15 @@
              [".gitignore"   (render "gitignore")]
              ["project.clj"  (render "project.clj" data)]
              ["README.md"    (render "README.md" data)]
-             "db"
              ["dev/dev.clj"  (render "dev/dev.clj" data)]
              ["dev/user.clj" (render "dev/user.clj")]
              ["resources/config.edn"  (render "resources/config.edn")]
              ["resources/logback.xml" (render "resources/logback.xml")]
-             "resources/sql"
              "resources/public"
              ["src/{{dirs}}/conf.clj" (render "src/conf.clj" data)]
              ["src/{{dirs}}/db.clj"   (render "src/db.clj" data)]
              ["src/{{dirs}}/main.clj" (render "src/main.clj" data)]
              ["src/{{dirs}}/www.clj"  (render "src/www.clj" data)]
              "src/{{dirs}}/db"
-             "src/{{dirs}}/handler")))
+             "src/{{dirs}}/handler"
+             "db" "sql")))
